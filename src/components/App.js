e513aee65bdac699f56b55e2cd2ba2a7145d26cc
warishshahbaz <mdshahbazwarish1996@gmail.com>
2024-03-07 13:07:20 +0530

    implemented

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,60 @@
-
 import React, { useState, useEffect } from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 
 const App = () => {
-    const cityList = [{ name: 'Goa', country: 'India' },
-    { name: 'Amsterdam', country: 'Netherlands' },
-    { name: 'New York', country: 'USA' },
-    { name: 'Darjeeling', country: 'India' },
-    { name: 'Tokyo', country: 'Japan' },
-    { name: 'Lonavala', country: 'India' },
-    { name: 'Brandenburg Gate', country: 'Germany' },
-    { name: 'Reichstag Building', country: 'Germany' },
-    { name: 'Museum Island', country: 'Germany' },
-    { name: 'Munnar', country: 'India' },
-    { name: 'Leh Ladakh', country: 'India' },
-    { name: 'Goa', country: 'India' },
-    { name: 'Agra', country: 'India' },
-    { name: 'Dalhousie', country: 'India' },
-    { name: 'Coorg', country: 'India' },
-    { name: 'Rome', country: 'Italy' },
-    { name: 'Milan', country: 'Italy' },
-    { name: 'Venice', country: 'Italy' },
-    { name: 'Varanasai', country: 'India' },
-    { name: 'Jaipur', country: 'India' },
-    { name: 'The Hofburg', country: 'Austria' },
-    { name: 'Belvedere Palace', country: 'Austria' },
-    { name: 'St. Stephen Cathedral', country: 'Austria' },
-    { name: 'Kahna National Park', country: 'India' },
-    { name: 'Amritsar', country: 'India' },
-    { name: 'Mussoorie', country: 'India' },
-    { name: 'Mount Abu', country: 'India' },
-    { name: 'Tirupati', country: 'India' },
-    ]
-    
+  const cityList = [
+    { name: "Goa", country: "India" },
+    { name: "Amsterdam", country: "Netherlands" },
+    { name: "New York", country: "USA" },
+    { name: "Darjeeling", country: "India" },
+    { name: "Tokyo", country: "Japan" },
+    { name: "Lonavala", country: "India" },
+    { name: "Brandenburg Gate", country: "Germany" },
+    { name: "Reichstag Building", country: "Germany" },
+    { name: "Museum Island", country: "Germany" },
+    { name: "Munnar", country: "India" },
+    { name: "Leh Ladakh", country: "India" },
+    { name: "Goa", country: "India" },
+    { name: "Agra", country: "India" },
+    { name: "Dalhousie", country: "India" },
+    { name: "Coorg", country: "India" },
+    { name: "Rome", country: "Italy" },
+    { name: "Milan", country: "Italy" },
+    { name: "Venice", country: "Italy" },
+    { name: "Varanasai", country: "India" },
+    { name: "Jaipur", country: "India" },
+    { name: "The Hofburg", country: "Austria" },
+    { name: "Belvedere Palace", country: "Austria" },
+    { name: "St. Stephen Cathedral", country: "Austria" },
+    { name: "Kahna National Park", country: "India" },
+    { name: "Amritsar", country: "India" },
+    { name: "Mussoorie", country: "India" },
+    { name: "Mount Abu", country: "India" },
+    { name: "Tirupati", country: "India" },
+  ];
+  const [state, setState] = useState([]);
+
+  useEffect(() => {
+    let result = cityList.filter((item) => item.country === "India");
+    setState(result);
+  }, []);
+
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      <ol>
+        {state.length > 0 &&
+          state.map((val, i) => {
+            return (
+              <div key={i}>
+                <li>
+                  {val.name} {val.country}
+                </li>
+              </div>
+            );
+          })}
+      </ol>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
